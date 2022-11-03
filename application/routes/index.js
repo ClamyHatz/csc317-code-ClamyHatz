41f07838a1363e2d4b149ee4e9e090f5db76663a
Lily Keus <104400465+ClamyHatz@users.noreply.github.com>
2022-11-02 17:38:26 -0700

    changed for m2

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Lily Keus" });
 });
 
+router.get("/login", function(req,res){
+  res.render('login')
+});
+router.get("/registration", function(req,res){
+  res.render('registration')
+});
+router.get("/postimage", function(req,res){
+  res.render('postimage')
+});
+router.get("/viewpost", function(req,res){
+  res.render('viewpost')
+});
+
 module.exports = router;
