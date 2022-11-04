2555a96183f7e3487ab5abbadd5c6db374f6109c
Lily Keus <104400465+ClamyHatz@users.noreply.github.com>
2022-11-04 16:22:23 -0700

    made a ton of css work with the handle bars

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,20 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Lily Keus" });
+  res.render('index', { title: 'CSC 317 App', name:"Lily Keus", css:["index.css"] });
 });
 
 router.get("/login", function(req,res){
-  res.render('login')
+  res.render('login', {css:["login.css"]})
 });
 router.get("/registration", function(req,res){
-  res.render('registration')
+  res.render('registration',{css:["registration.css"]})
 });
 router.get("/postimage", function(req,res){
-  res.render('postimage')
+  res.render('postimage',{css:["postimage.css"]})
 });
 router.get("/viewpost", function(req,res){
-  res.render('viewpost')
+  res.render('viewpost',{css:["viewpost.css"]})
 });
 
 module.exports = router;
