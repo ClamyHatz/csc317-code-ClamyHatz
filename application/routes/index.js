2dad7a61cd5499f4e46483cbddf9fe0554aeea00
Lily Keus <104400465+ClamyHatz@users.noreply.github.com>
2022-11-04 17:58:29 -0700

    finished milestone 2

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Lily Keus", css:["index.css"] });
+  res.render('index', { title: 'CSC 317 App', name:"Lily Keus", css:["index.css"], js:["Index.js"] });
 });
 
 router.get("/login", function(req,res){
   res.render('login', {css:["login.css"]})
 });
 router.get("/registration", function(req,res){
-  res.render('registration',{css:["registration.css"]})
+  res.render('registration',{css:["registration.css"], js:["registration.js"]})
 });
 router.get("/postimage", function(req,res){
   res.render('postimage',{css:["postimage.css"]})
