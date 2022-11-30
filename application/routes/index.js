d36b96474848da26c525dbf6295d128131a6bbfb
Lily Keus <104400465+ClamyHatz@users.noreply.github.com>
2022-11-29 19:25:24 -0800

    fixed a lot css and made interface adjustments

--- application/routes/index.js
+++ application/routes/index.js
@@ -4,21 +4,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Lily Keus", css:["index.css"], js:["Index.js"] });
+  res.render('index', { title: 'CSC 317 App', name:"Lily Keus", css:["white.css"]});
 });
 
 router.get("/login", function(req,res){
-  res.render('login', {css:["login.css"]})
+  res.render('login', {css:["blue.css"]})
 });
 router.get("/registration", function(req,res){
-  res.render('registration',{css:["registration.css"]})
+  res.render('registration',{css:["blue.css"]})
 });
 
 router.get("/postimage", isLoggedIn, function(req,res){
-  res.render('postimage',{css:["postimage.css"]})
+  res.render('postimage',{css:["pink.css"]})
 });
 router.get("/viewpost", function(req,res){
-  res.render('viewpost',{css:["viewpost.css"]})
+  res.render('viewpost',{css:["pink.css"]})
 });
 
 module.exports = router;
