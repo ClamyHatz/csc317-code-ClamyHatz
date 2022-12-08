284419bc3b81f399e168ba92fde9f0f2a40b5fe7
Lily Keus <104400465+ClamyHatz@users.noreply.github.com>
2022-12-07 18:58:03 -0800

    qweqwe

--- application/routes/users.js
+++ application/routes/users.js
@@ -99,18 +99,18 @@ router.post('/login', function(req,res,next){
 
 router.post("/logout", function(req, res, next){
   req.flash("success", 'Log Out Successful!')
-    req.session.save(function(saveError){
-      req.session.destroy(function(destroyError){
-        if(destroyError){
-          next(err);
-        } else {
-          res.json({
-            status: 200,
-            message: "You have been logged out"
-          });
-        }
-      })
+  req.session.save(function(saveError){
+    req.session.destroy(function(destroyError){
+      if(destroyError){
+        next(err);
+      } else {
+        res.json({
+          status: 200,
+          message: "You have been logged out"
+        });
+      }
     })
+  })
 })
 
 module.exports = router;
